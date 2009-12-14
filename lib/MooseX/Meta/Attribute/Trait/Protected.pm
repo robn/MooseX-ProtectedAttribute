f6f29b08b3804fb4a6867ac1f26267f7605a08b8
Robert Norris <rob@cataclysm.cx>
2009-12-15 09:15:52 +1100

    code stubs

--- lib/MooseX/Meta/Attribute/Trait/Protected.pm
+++ lib/MooseX/Meta/Attribute/Trait/Protected.pm
@@ -1,3 +1,11 @@
 package MooseX::Meta::Attribute::Trait::Protected;
 
+use Moose::Role;
+
+has "protection" => (
+    is      => "rw",
+    isa     => "Str",
+    default => "private",
+);
+
 1;
--- lib/MooseX/ProtectedAttribute.pm
+++ lib/MooseX/ProtectedAttribute.pm
@@ -1,3 +1,24 @@
 package MooseX::ProtectedAttribute;
 
+use strict;
+use warnings;
+
+use Moose ();
+use Moose::Exporter ();
+
+use MooseX::Meta::Attribute::Trait::Protected;
+
+our $VERSION = '0.01';
+
+Moose::Exporter->setup_import_methods(
+    with_caller => [qw(has_protected has_private)],
+    also        => "Moose",
+);
+
+sub has_protected {
+}
+
+sub has_private {
+}
+
 1;
