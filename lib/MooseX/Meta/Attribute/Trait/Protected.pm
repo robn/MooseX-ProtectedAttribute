9cb4846f77031d47b82a4f31d2f12505258cf13f
Robert Norris <rob@cataclysm.cx>
2009-12-15 09:29:23 +1100

    _process_accessors wrapper from the prototype

--- lib/MooseX/Meta/Attribute/Trait/Protected.pm
+++ lib/MooseX/Meta/Attribute/Trait/Protected.pm
@@ -8,6 +8,34 @@ has "protection" => (
     default => "private",
 );
 
+around "_process_accessors" => sub {
+    my $orig = shift;
+    my $self = shift;
+
+    print "PA: _process_accessors\n";
+
+    print "    orig: $orig\n";
+    print "    self: $self\n";
+
+    if (@_) {
+        print "    args:\n";
+        print "        $_\n" for map { defined $_ ? $_ : "[undef]" } @_;
+    }
+
+    my ($name, $method) = $self->$orig(@_);
+
+    my $wrap = Class::MOP::Method::Wrapped->wrap($method);
+    $wrap->add_around_modifier(sub {
+        my $orig = shift;
+
+        print ">>> I'm around $name\n";
+
+        goto $orig;
+    });
+
+    return ($name, $wrap);
+};
+
 no Moose::Role;
 
 package # hide from PAUSE
